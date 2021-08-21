b6b9af6acb74297cf5d2b2b4db2fda46e9e82ea7
Mir Masudur Rhaman <mirmrhaman@gmail.com>
2021-08-21 00:29:39 -0400

    update

---
+++ src/test/java/listeners/BaseClass.java
@@ -0,0 +1,41 @@
+package listeners;
+
+import command_providers.ActOn;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.ThreadContext;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import utilities.ReadConfigFiles;
+import utilities.TestEnvironment;
+
+import java.net.MalformedURLException;
+import java.util.logging.Logger;
+
+public class BaseClass {
+    public static WebDriver driver;
+    Logger LOGGER = (Logger) LogManager.getLogger(this.getClass().getName());
+    String testCaseName = String.format("------Test: %s------", this.getClass().getName());
+    String endTestCase = String.format("-----Test End: %s-----", this.getClass().getName());
+
+    @BeforeClass
+    public void addThread() {
+        ThreadContext.put("threadName", this.getClass().getName());
+    }
+
+    @BeforeMethod
+    public void openBrowser() throws MalformedURLException{
+        String url = ReadConfigFiles.getPropertyValue("Url");
+        driver = TestEnvironment.selectTestExecutionEnvironment();
+        LOGGER.info(testCaseName);
+        LOGGER.debug("Open the URL: " + url);
+        ActOn.browser(driver).openBrowser(url);
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        ActOn.browser(driver).closeBrowser();
+        LOGGER.info(endTestCase);
+    }
+}
---
+++ src/test/java/listeners/LoggerForParallelTests.java
@@ -0,0 +1,12 @@
+package listeners;
+
+
+import org.apache.logging.log4j.ThreadContext;
+import org.testng.annotations.BeforeClass;
+
+public class LoggerForParallelTests {
+    @BeforeClass
+    public void addThread() {
+        ThreadContext.put("threadName", this.getClass().getName());
+    }
+}
